1b038d6c0b9032a8c0a8ded9923c1e5928a86995
DuP-491 <divyansh.20198117@mnnit.ac.in>
2022-04-20 12:14:25 +0530

    item model

---
+++ backend/hmsBackend/Mess/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+
+##### Tables ######
+# Item (id, name, brand, desc, category)
+# Category (id, name, desc)
+
+class Category(models.Model):
+    name = models.CharField(null=False, max_length=20, blank=False, unique=True)
+    desc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    name = models.CharField(null=False, max_length=50, blank=False, unique=True)
+    brand = models.CharField(max_length=50, default="Unknown")
+    desc = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
