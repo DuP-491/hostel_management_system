3211ef7bb0081bdede7e2a2da8dfaae73a10e4e4
DuP-491 <divyansh.20198117@mnnit.ac.in>
2022-04-26 22:05:32 +0530

    drf setup

--- backend/hmsBackend/hmsBackend/settings.py
+++ backend/hmsBackend/hmsBackend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Mess',
+    #third party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
